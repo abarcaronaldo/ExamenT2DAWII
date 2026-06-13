c0d267d635f099322c94ff692fbf922cc5e4035a
Ronaldo <abarcaronaldo18@gmail.com>
2026-06-13 18:00:25 -0500

    feat: estructura proyecto

---
+++ ms-pacientes/src/main/java/com/edu/ms_pacientes/MsPacientesApplication.java
@@ -0,0 +1,13 @@
+package com.edu.ms_pacientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPacientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPacientesApplication.class, args);
+	}
+
+}
